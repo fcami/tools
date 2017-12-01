59f8065a0a331421eb44b4df1554ffeb1dd38560
François Cami <fcami@fedoraproject.org>
2017-12-01 17:22:49 +0100

    add gnome-terminal + gdbus launcher

---
+++ dbus/gt.py
@@ -0,0 +1,37 @@
+#!/bin/bin/python
+
+# Copyright (c) 2017 François Cami
+
+# demonstrate how to launch a gnome-terminal monitored by gdbus
+# best launched in dbus-run-session:
+# $ dbus-run-session -- gt.py
+
+import subprocess
+
+from threading import Thread
+import re
+
+cmd2=['gnome-terminal']
+cmd1=['gdbus', 'monitor', '--session', '-d', 'org.gnome.Terminal']
+
+def launch(cmd):
+  p = subprocess.Popen(cmd,
+                     stdout=subprocess.PIPE)
+  while True:
+      output = p.stdout.readline()
+      if output == '' and p.poll() is not None:
+          break
+      if output:
+          matches = re.findall('The name org.gnome.Terminal does not have an owner', output, re.DOTALL)
+          if len(matches) > 0:
+		p.kill()
+  rc = p.poll()
+
+
+
+t1 = Thread(target=launch, args=(cmd1,))
+t2 = Thread(target=launch, args=(cmd2,))
+t1.start()
+t2.start()
+
+
